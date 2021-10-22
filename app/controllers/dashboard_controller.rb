5a62e8c00d2262d432c8874e787f5bc9a4417754
hbeita <hecbeita1988@gmail.com>
2021-10-22 15:45:13 -0600

    Seeds admins I don't want to register people yet and a little style (not much).

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class DashboardController < ApplicationController
   def index
+    render :index
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'securerandom'
+
+def seed_admins
+  admins = [
+    'hecbeita1988@gmail.com',
+    'admin@acmv.com',
+  ]
+
+  admins.each do |email|
+    User.find_or_initialize_by(email: email) do |user|
+      user.email    = email
+      user.password = SecureRandom.hex(8)
+      user.save
+    end
+  end
+end
+
+seed_admins
