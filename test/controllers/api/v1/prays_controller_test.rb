3347dc3f37fae23544098720f4e4f84ba8e20cae
hbeita <hecbeita1988@gmail.com>
2021-10-23 06:35:39 -0600

    Rubocop happy.

--- test/controllers/api/v1/prays_controller_test.rb
+++ test/controllers/api/v1/prays_controller_test.rb
@@ -1,19 +1,25 @@
-require "test_helper"
+# frozen_string_literal: true
 
-class Api::V1::PraysControllerTest < ActionDispatch::IntegrationTest
-  def test_create_endpoint_returns_saves_new_pray
-    assert_difference "Pray.count" do
-      post api_v1_prays_create_path(
-        {
-          pray: {
-            submitted_by: "Hector",
-            pray: "I need a pray for Joe Doe he needs it."
+require 'test_helper'
 
-          }
-        }
-      )
+module Api
+  module V1
+    class PraysControllerTest < ActionDispatch::IntegrationTest
+      def test_create_endpoint_returns_saves_new_pray
+        assert_difference 'Pray.count' do
+          post api_v1_prays_create_path(
+            {
+              pray: {
+                submitted_by: 'Hector',
+                pray: 'I need a pray for Joe Doe he needs it.'
+
+              }
+            }
+          )
+        end
+
+        assert_response :success
+      end
     end
-
-    assert_response :success
   end
 end
