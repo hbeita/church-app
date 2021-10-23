623ba7082e995cbd0912ebad3e8d880856816243
hbeita <hecbeita1988@gmail.com>
2021-10-23 05:46:02 -0600

    Tests

---
+++ test/controllers/prays_controller_test.rb
@@ -0,0 +1,48 @@
+require "test_helper"
+
+class PraysControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @pray = prays(:one)
+  end
+
+  test "should get index" do
+    get prays_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_pray_url
+    assert_response :success
+  end
+
+  test "should create pray" do
+    assert_difference('Pray.count') do
+      post prays_url, params: { pray: { checked: @pray.checked, pray: @pray.pray, submitted_by: @pray.submitted_by } }
+    end
+
+    assert_redirected_to pray_url(Pray.last)
+  end
+
+  test "should show pray" do
+    get pray_url(@pray)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_pray_url(@pray)
+    assert_response :success
+  end
+
+  test "should update pray" do
+    patch pray_url(@pray), params: { pray: { checked: @pray.checked, pray: @pray.pray, submitted_by: @pray.submitted_by } }
+    assert_redirected_to pray_url(@pray)
+  end
+
+  test "should destroy pray" do
+    assert_difference('Pray.count', -1) do
+      delete pray_url(@pray)
+    end
+
+    assert_redirected_to prays_url
+  end
+end
