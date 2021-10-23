20443e6e9f16e2936e2d7bda8d04faf6b4cac5fb
hbeita <hecbeita1988@gmail.com>
2021-10-23 05:43:00 -0600

    Add pray model migration.

---
+++ db/migrate/20211023113534_create_prays.rb
@@ -0,0 +1,11 @@
+class CreatePrays < ActiveRecord::Migration[6.1]
+  def change
+    create_table :prays do |t|
+      t.boolean :checked, default: false
+      t.string :submitted_by
+      t.string :pray
+
+      t.timestamps
+    end
+  end
+end
